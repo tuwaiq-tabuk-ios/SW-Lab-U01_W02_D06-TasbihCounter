fa24f7ff1f1a0d09d0f491e47e1fca5111d877e6
bushra nazal alatwi <bushran486gmail.com@MacBook-Pro-bushra.local>
2021-10-10 21:08:37 +0300

    TasbihCounter

---
+++ TasbihCounter/TasbihCounter/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  TasbihCounter
+//
+//  Created by bushra nazal alatwi on 04/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    
+    var counter = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+   
+    @IBAction func discountPressd(_ sender: UIButton) {
+        subOnToCounter()
+        
+    }
+    
+    @IBAction func counterPreessd(_ sender: UIButton) {
+        addOnToCounter()
+    }
+    
+    @IBAction func reast(_ sender: UIButton) {
+        removeOnToCounter()
+        
+    }
+    
+    
+    func addOnToCounter(){
+      counter += 1
+        //displayLabel.text = "5"
+        //displayLabel.text = counter.description
+        displayLabel.text = counter.description
+        print("Number displayed:\(counter)")
+        
+    }
+    func subOnToCounter(){
+      
+        if (counter > 0){
+            counter -= 1
+            
+        }
+    
+        displayLabel.text = counter.description
+        print("Number displayed:\(counter)")
+    }
+    func removeOnToCounter(){
+      counter = 0
+        //displayLabel.text = "5"
+        //displayLabel.text = counter.description
+        displayLabel.text = counter.description
+        print("Number displayed:\(counter)")    }
+}
+
