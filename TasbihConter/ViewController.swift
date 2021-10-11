12f3435fb31bee9e8f02d0caa7b01340069c73b6
عبدالعزيز البلوي <bdalzyzalblwy@MacBook-Air-bdalzyz.local>
2021-10-11 10:47:06 +0300

    finsh

--- TasbihConter/ViewController.swift
+++ TasbihConter/ViewController.swift
@@ -8,56 +8,65 @@
 import UIKit
 
 class ViewController: UIViewController, UIColorPickerViewControllerDelegate{
-
+  
+  
+  
+  @IBOutlet weak var disPlayLabel: UILabel!
+  
+  let colorPicker = UIColorPickerViewController()
+  var counter = 0
+  
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    colorPicker.delegate = self
+    // Do any additional setup after loading the view.
+  }
+  
+  @IBAction func discountPressed(_ sender: UIButton) {
+    discountOneToCount()
+  }
+  
+  @IBAction func countarPressed(_ sender: UIButton) {
+    addOneToCounter()
+  }
+  
+  
+  @IBAction func restPressed(_ sender: UIButton) {
+    counter = 0
+    disPlayLabel.text = counter.description
+    print("Number displayd: \(counter)")
+  }
+  
+  @IBAction func clolrPressed(_ sender: UIButton) {
+    
+    present(colorPicker, animated: true, completion: nil)
     
     
-    @IBOutlet weak var displaylable: UILabel!
-    
-    let colorPicker = UIColorPickerViewController()
-    
-    var counter = 0
- 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        colorPicker.delegate = self
-        // Do any additional setup after loading the view.
-    }
-
-    @IBAction func discountpressed(_ sender: UIButton) {
-        if counter != 0 {
-        counter -= 1
-        displaylable.text = counter.description
-        print("Number displayd: \(counter)")
-        }
-    }
-    
-    @IBAction func contarpressed(_ sender: UIButton) {
+  }
+  
+  
+  
+  func colorPickerViewControllerDidSelectColor(_ viewController: UIColorPickerViewController) {
+    self.view?.backgroundColor = colorPicker.selectedColor
+  }
+  
+  func addOneToCounter (){
     // countre = counter + 1
-        counter += 1
-        //    displaylable.text = "5"
-        displaylable.text = counter.description
-        print("Number displayd: \(counter)")
-        
+    counter += 1
+    //    displaylable.text = "5"
+    disPlayLabel.text = counter.description
+    print("Number displayd: \(counter)")
+  }
+  func discountOneToCount(){
+    if counter != 0 {
+      counter -= 1
+      disPlayLabel.text = counter.description
+      print("Number displayd: \(counter)")
     }
-   
-    
-    @IBAction func restpressed(_ sender: UIButton) {
-        counter = 0
-        displaylable.text = counter.description
-        print("Number displayd: \(counter)")
-    }
-    
-    @IBAction func clolrpressed(_ sender: UIButton) {
-        
-        present(colorPicker, animated: true, completion: nil)
-        
-        
-    }
-    
-    func colorPickerViewControllerDidSelectColor(_ viewController: UIColorPickerViewController) {
-        self.view?.backgroundColor = colorPicker.selectedColor
-    }
-    
-    
+  }
+  
+  
 }
 
