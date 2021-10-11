69d25bc646b123d8544d9d89f245f703f76f6405
Mohammed <mohammed@Mohammeds-MacBook-Air.local>
2021-10-11 15:19:05 +0300

    Update the codes

--- TasbihCounter/TasbihCounter/ViewController.swift
+++ TasbihCounter/TasbihCounter/ViewController.swift
@@ -8,81 +8,77 @@
 import UIKit
 
 class ViewController: UIViewController , UIColorPickerViewControllerDelegate{
-    
- @IBOutlet weak var DisplayLabel: UILabel!
-    //var counter: Int = 0
-    @IBOutlet weak var TitleLabel: UILabel!
-    
-    @IBOutlet weak var viewColor: UIView!
-    
-    var counter = 0
-    let ColorPicker = UIColorPickerViewController()
-
-    
-   
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        ColorPicker.delegate = self;
-        // Do any additional setup after loading the view.
-    }
-    
-    @IBAction func DiscountPressed(_ sender: UIButton) {
-        if counter != 0 {
-            counter -= 1
-        DisplayLabel.text = counter.description
-        print("Number displayed: \(counter)")
-        }
-    }
-      
-    
-    
-    
-    @IBAction func CountPressed(_ sender: UIButton) {
-        addOneToCounter()
-    
-    }
-    
-    
-    func addOneToCounter() {
-    //    counter = counter + 1
-        counter += 1
-    //    displayLebal.text = "5"
-        
-        DisplayLabel.text = counter.description
-        print("Number displayed\(counter)")
-    }
-    
-    
-    @IBAction func RestartButton(_ sender: Any) {
-        counter = 0
-        DisplayLabel.text = counter.description
-    }
-    
-    
-    
-    
-    
-    @IBAction func ColourButton(_ sender: UIButton) {
-       
-        self.present(ColorPicker, animated: true, completion: nil)
-        //TitleLabel.textColor = ColorPicker.selectedColor
-    }
-    
-    func colorPickerViewControllerDidSelectColor(_ viewController: UIColorPickerViewController) {
-        self.view.backgroundColor = ColorPicker.selectedColor
-        viewColor.backgroundColor = ColorPicker.selectedColor
   
-    
+  @IBOutlet weak var displayLabel: UILabel!
+  @IBOutlet weak var titleLabel: UILabel!
+  
+  
+  var counter = 0
+  let colorPicker = UIColorPickerViewController()
+  
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    colorPicker.delegate = self;
+  }
+  
+  @IBAction func discountButton(_ sender: UIButton) {
+    if counter != 0 {
+      counter -= 1
+      displayLabel.text = counter.description
+      print("Number displayed: \(counter)")
     }
+  }
+  
+  @IBAction func countButton(_ sender: UIButton) {
+    addOneToCounter()
+  }
+  
+  
+  @IBAction func restartButton(_ sender: Any) {
+    counter = 0
+    displayLabel.text = counter.description
+  }
+  
+  
+  @IBAction func colourButton(_ sender: UIButton) {
+    self.present(colorPicker, animated: true, completion: nil)
+  }
+  
+  
+  func colorPickerViewControllerDidSelectColor(_ viewController: UIColorPickerViewController) {
+    self.view.backgroundColor = colorPicker.selectedColor
+  }
+  
+  
+  func discountOnefromCounter() {
+    if counter != 0 {
+      counter -= 1
+      displayLabel.text = counter.description
+      print("Number displayed: \(counter)")
+    }
+  }
+  
+  
+  func addOneToCounter() {
+    counter += 1
+    
+    displayLabel.text = counter.description
+    print("Number displayed\(counter)")
+  }
+  
+  
+  func resetCounter() {
+    counter = 0
+    displayLabel.text = counter.description
+  }
+  
+  
+  func colourButton() {
+    
+    self.present(colorPicker, animated: true, completion: nil)
+  }
+
 }
-
-
-
-
-
-
-
-
-
 
