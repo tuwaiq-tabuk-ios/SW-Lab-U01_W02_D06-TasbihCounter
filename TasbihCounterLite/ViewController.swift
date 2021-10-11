94dff8fb9aa85972724a2e03bd2930b3c3074561
azooz alhwiti <azoozalhwiti@MacBook-Air-azooz.local>
2021-10-11 10:59:50 +0300

    update tasbihCounter

--- TasbihCounterLite/ViewController.swift
+++ TasbihCounterLite/ViewController.swift
@@ -8,47 +8,64 @@
 import UIKit
 
 class ViewController: UIViewController , UIColorPickerViewControllerDelegate {
-
-    @IBOutlet weak var DisplayLabel: UILabel!
-    let coler = UIColorPickerViewController()
-    var counter = 0
+  
+  @IBOutlet weak var displayLabel: UILabel!
+  
+  
+  let coler = UIColorPickerViewController()
+  var counter = 0
+  
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    coler.delegate = self
+    // Do any additional setup after loading the view.
+  }
+  
+  
+  @IBAction func counterPreased(_ sender: Any) {
+    addOneToCounter()
+  }
+  
+  @IBAction func resetPreased(_ sender: Any) {
+    resetPreased()
+  }
+  
+  @IBAction func discountPreased(_ sender: Any) {
+    discountOneToCounter()
+  }
+  
+  @IBAction func colrpreased(_ sender: UIButton) {
+    present(coler,animated: true,completion: nil)
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        coler.delegate = self
-        // Do any additional setup after loading the view.
-    }
+  }
+  
+  
+  func
+  colorPickerViewControllerDidSelectColor(_ viewController: UIColorPickerViewController) {
+    self.view?.backgroundColor = coler.selectedColor
+  }
+  
+  func addOneToCounter(){
+    counter += 1
     
-    
-    @IBAction func CounterPreased(_ sender: Any) {
-        
-        counter += 1
-        
-        DisplayLabel.text = counter.description
-        
+    displayLabel.text = counter.description
+  }
+  
+  func discountOneToCounter(){
+    if counter != 0 {
+      counter -= 1
+      displayLabel.text = counter.description
     }
-    
-    @IBAction func resetPreased(_ sender: Any) {
-        counter = 0
-        DisplayLabel.text = counter.description
-        
-        
-    }
-    
-    @IBAction func discountPreased(_ sender: Any) {
-        if counter != 0 {
-        counter -= 1
-        DisplayLabel.text = counter.description
-        }
-    }
-    
-    @IBAction func colrpreased(_ sender: UIButton) {
-      present(coler,animated: true,completion: nil)
-        
-    }
-    func
-    colorPickerViewControllerDidSelectColor(_ viewController: UIColorPickerViewController) {
-        self.view?.backgroundColor = coler.selectedColor
-    }
+  }
+  
+  func resetPreased(){
+    counter = 0
+    displayLabel.text = counter.description
+  }
+  
+  
+  
 }
 
