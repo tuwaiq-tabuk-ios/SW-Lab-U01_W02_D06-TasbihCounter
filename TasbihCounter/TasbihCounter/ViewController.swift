8d3e1a7210ea166490179c189475bc303dbcd34b
Yousef Albalawi <yousef@Yousefs-MacBook-Air.local>
2021-10-11 11:00:23 +0300

    finshd

--- TasbihCounter/TasbihCounter/ViewController.swift
+++ TasbihCounter/TasbihCounter/ViewController.swift
@@ -8,59 +8,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    
-    
-    
-    @IBOutlet weak var Display_laipl: UILabel!
-    
-    // var countr: int = 0
-    var counter = 0
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-    
-    @IBAction func discountPreased(_ sender: UIButton) {
-        if counter != 0 {
-        counter -= 1
-        
-        Display_laipl.text = counter.description
-        print("Number displayd: \(counter)")
-    
-        }
-    }
-    
-    
-    @IBAction func ContarPresad(_ sender: UIButton) {
-        
-      
-            // counter = counter  = + 1
-            counter += 1
-            // Display_laipl.text = "5"
-            
-            
-            Display_laipl.text = counter.description
-            print("Number displayd: \(counter)")
-        
-        
-        
-        
-    }
-    
-    
-    @IBAction func Rst_Presad(_ sender: Any) {
-        counter = 0
-        
-        Display_laipl.text = counter.description
-        
-    }
-    
   
-    }
+  
+  
+  
+  @IBOutlet weak var display_laipl: UILabel!
+  
+  
+  // var countr: int = 0
+  var counter = 0
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  
+  @IBAction func discountPreased(_ sender: UIButton) {
+    discountOneToCounter()
+  }
+  
+  
+  @IBAction func contarPresad(_ sender: UIButton) {
+    addOneToCounter()
+  }
+  
+  
+  @IBAction func resetPresad(_ sender: Any) {
+    counter = 0
     
-
-
+    display_laipl.text = counter.description
+  }
+  
+  
+  
+  func addOneToCounter() {
+    // counter = counter  = + 1
+    counter += 1
+    // Display_laipl.text = "5"
+    
+    display_laipl.text = counter.description
+    print("Number displayd: \(counter)")
+  }
+  
+  
+  func discountOneToCounter() {
+    if counter != 0 {
+      counter -= 1
+      
+      display_laipl.text = counter.description
+      print("Number displayd: \(counter)")
+      
+    }
+  }
+  
+  
+}
